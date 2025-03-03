10121bf9886e3c9d6eafd30bba0a39a389c5b6b8
Fareeday <fareed_farooq@hotmail.com>
2025-03-03 16:43:33 +0500

    default values are added, it won't ask user for ECS_Service Name, Cluser Name, ALB Name, Image Name

--- variables.tf
+++ variables.tf
@@ -1,36 +1,41 @@
 variable "service_name" {
-  description = "ECS Service name"
+  description = "ECS Service Name"
   type        = string
+  default     = "ExampleService"
 }
 
 variable "cluster_name" {
-  description = "ECS Service name"
+  description = "ECS Cluster Name"
   type        = string
+  default     = "ExampleCluster"
 }
 
 variable "container_name" {
-  description = "ECS container name"
+  description = "ECS Conntainer Name"
   type        = string
+  default     = "ExampleContainer"
 }
 
 variable "container_port" {
-  description = "ECS Service name"
+  description = "ECS Container Port Number"
   type        = string
   default     = 80
 }
 
 variable "target_port" {
-  description = "ECS Service name"
+  description = "ECS Target Port Number"
   type        = string
   default     = 80
 }
 
 variable "alb_name" {
-  description = "ECS Service name"
+  description = "ALB Name"
   type        = string
+  default     = "ExampleLB"
 }
 
 variable "image_name" {
-  description = "ECS Service name"
+  description = "ECS Container Image Name"
   type        = string
+  default     = "nginx:latest"
 }
